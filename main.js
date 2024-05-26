18211e231c8eba8496712245f92558dc74670904
miriam <miriammugure610@gmail.com>
2024-05-26 13:14:36 +0300

    add a hamburger menu to the responsive site

---
+++ main.js
@@ -0,0 +1,14 @@
+const hambuger = document.querySelector( ".hamburger");
+const header_navlist = document.querySelector(".header-nav-list");
+
+hambuger.addEventListener("click", () => {
+  hambuger.classList.toggle("active");
+  header_navlist.classList.toggle("active");
+});
+
+document.querySelectorAll(".header-nav-link").forEach((n) =>
+  n.addEventListener("click", () => {
+    hambuger.classList.remove("active");
+    header_navlist.classList.remove("active");
+  }),
+);
